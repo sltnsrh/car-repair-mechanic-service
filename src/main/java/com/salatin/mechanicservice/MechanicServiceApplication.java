05f610aee6b5f0f819c962da3941410cd04188ce
Serhii Salatin <lifenjoy8888@gmail.com>
2023-02-20 12:36:08 +0200

    was deleted redundant package

--- src/main/java/com/salatin/mechanicservice/MechanicServiceApplication.java
+++ src/main/java/com/salatin/mechanicservice/MechanicServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ssalatin.mechanicservice;
+package com.salatin.mechanicservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
